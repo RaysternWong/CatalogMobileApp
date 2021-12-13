f558456ba95aefac49d8249c451a98f62dc99c85
Raystern <raysternwong@gmail.com>
2021-12-13 14:20:29 +0800

    Add project files.

---
+++ CatalogMobileApp/CatalogMobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CatalogMobileApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CatalogMobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
